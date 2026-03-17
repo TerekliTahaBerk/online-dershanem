d65782fd447032dd0afdef04ce304b1e3c9bd2ba
Taha Berk <tterekli9@gmail.com>
2026-03-17 11:12:25 +0300

    Remove env configuration from next.config.ts
    Removed environment variable configuration for NEXT_PUBLIC_BASE_PATH.

--- next.config.ts
+++ next.config.ts
@@ -13,9 +13,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath
-  },
   basePath,
   assetPrefix: basePath || undefined
 };
